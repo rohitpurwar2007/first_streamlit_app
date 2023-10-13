f5a3808c27a248189dcecf0249de8524fe266198
rohitpurwar2007 <49224962+rohitpurwar2007@users.noreply.github.com>
2023-10-13 20:14:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,9 @@ streamlit.text(my_data_row)
 
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchall()
+streamlit.header("the fruit load list contains:")
+streamlit.dataframe(my_data_row)
